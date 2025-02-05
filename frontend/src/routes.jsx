2d1fbd5d5434df1d0be821dd35ee4c9e7556bcc8
NamoxKalvium <Namonarayan.Mina@Kalvium.community>
2025-02-05 16:40:48 +0530

    milestone 10

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,5 +1,5 @@
 import LoginPage from "./pages/Login"
 import SignupPage from "./pages/Signup"
 import Home from "./pages/Home"
-import CreateProduct from "./pages/createProduct.jsx"
+import CreateProduct from "./pages/CreateProduct.jsx"
 export { LoginPage, SignupPage, Home , CreateProduct };
