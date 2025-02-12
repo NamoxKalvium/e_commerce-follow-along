77f1985f9b48723ebd26e89fa772d1e84d91e39e
NamoxKalvium <Namonarayan.Mina@Kalvium.community>
2025-02-12 10:22:37 +0530

    milestone 15 and 16 done

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,6 +1,8 @@
-import LoginPage from "./Pages/Login";
-import SignupPage from "./Pages/SignUp";
-import Home from "./Pages/Home";
-import CreateProduct from "./Pages/createProduct";
-import MyProducts from "./Pages/myProduct";
-export {LoginPage, SignupPage, Home, CreateProduct, MyProducts};
+import LoginPage from "./pages/Login.jsx";
+import SignupPage from "./pages/Signup.jsx";
+import Home from "./pages/Home.jsx";
+import CreateProduct from './pages/createProduct.jsx';
+import MyProducts from "./pages/myProducts.jsx";
+import Cart from "./pages/cart.jsx";
+import ProductDetails from "./pages/productDetails.jsx";
+export { SignupPage, LoginPage, Home, CreateProduct, MyProducts, Cart, ProductDetails };
